bc229319648fd7ea8db092f781fc0efc93b33eeb
Satoru Nabeshima <snabeshi@chicagobooth.edu>
2023-05-15 14:42:40 +0000

    Currency conversion done

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -13,7 +13,7 @@ class CurrenciesController < ActionController::Base
   end
 
   def select_currency
-    @currency = params.fetch("symbols")
+    @from_currency = params.fetch("from_currency")
     api_endpoint = "https://api.exchangerate.host/symbols"
     require "open-uri"
     raw_response = URI.open(api_endpoint).read
@@ -25,4 +25,22 @@ class CurrenciesController < ActionController::Base
     
     render({:template => "currency_templates/convert_currency.html.erb"})
   end
+
+  def convert_currency
+    @from_currency = params.fetch("from_currency")
+    @to_currency = params.fetch("to_currency")
+    api_endpoint = "https://api.exchangerate.host/convert?from=#{@from_currency}&to=#{@to_currency}"
+    require "open-uri"
+    raw_response = URI.open(api_endpoint).read
+    
+    require "json"
+    parsed_response = JSON.parse(raw_response)
+  
+    info_array = parsed_response.fetch("info")
+    @rate = info_array.fetch("rate")
+    
+    render({:template => "currency_templates/show_conversion.html.erb"})
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get("/forex", {:controller => "currencies", :action => "pair_currencies"})
-  get("/forex/:symbols", {:controller => "currencies", :action => "select_currency"})
+  get("/forex/:from_currency", {:controller => "currencies", :action => "select_currency"})
+  get("/forex/:from_currency/:to_currency", {:controller => "currencies", :action => "convert_currency"})
+
 end
